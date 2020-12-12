944e0113c5cd17ec12a18065cc2dafeec0ee4900
hp_desktop <hp_desktop@qq.com>
2020-12-13 01:45:57 +0800

    leetcode 双周赛

---
+++ _Todo/5610_Sum-of-Absolute-Differences-in-a-Sorted-Array.cpp
@@ -0,0 +1,7 @@
+/*
+Title:
+    5610. Sum of Absolute Differences in a Sorted Array
+    5610. 有序数组中差绝对值之和
+Address: 
+    https://leetcode-cn.com/contest/biweekly-contest-41/problems/sum-of-absolute-differences-in-a-sorted-array/
+*/
