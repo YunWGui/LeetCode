0a78b63e7d0fb033ec47f1af3efa13bc389e48de
云忘归 <58332217+YunWGui@users.noreply.github.com>
2021-02-06 17:30:52 +0800

    349. Intersection of Two Arrays

---
+++ _Todo/349_Intersection-of-Two-Arrays/349_Intersection-of-Two-Arrays.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+ * Title:
+ *     349. Intersection of Two Arrays
+ *     349. 两个数组的交集
+ * Address:
+ *     https://leetcode-cn.com/problems/intersection-of-two-arrays/
+******************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+// 方法一：哈希表
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_set<int> hashSet1( nums1.begin(), nums1.end() );
+        unordered_set<int> hashSet2( nums2.begin(), nums2.end() );
+
+        for ( auto iter = hashSet1.begin(); iter != hashSet1.end(); ++iter ) {
+            if ( hashSet2.count( *iter ) )
+                ans.push_back( *iter );
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
---
+++ _Todo/349_Intersection-of-Two-Arrays/349_Intersection-of-Two-Arrays.py
@@ -0,0 +1,19 @@
+"""
+ * Title:
+ *     349. Intersection of Two Arrays
+ *     349. 两个数组的交集
+ * Address:
+ *     https://leetcode-cn.com/problems/intersection-of-two-arrays/
+"""
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = list()
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        for s1 in set1:
+            if s1 in set2:
+                ans.append(s1)
+        
+        return ans
