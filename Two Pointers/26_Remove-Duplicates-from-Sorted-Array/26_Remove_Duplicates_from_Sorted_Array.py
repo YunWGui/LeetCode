8d324b1ce90386bd9f3074c9b6cb17f177343e58
hp_desktop <hp_desktop@qq.com>
2021-04-18 01:00:48 +0800

    26. 删除排序数组的重复项

--- Two Pointers/26_Remove-Duplicates-from-Sorted-Array/26_Remove_Duplicates_from_Sorted_Array.py
+++ Two Pointers/26_Remove-Duplicates-from-Sorted-Array/26_Remove_Duplicates_from_Sorted_Array.py
@@ -14,10 +14,32 @@ Address:
 	https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
 """
 
-
+# 方法一：
 class Solution:
     def removeDuplicates(self, nums):
         return list(set(nums))      
+
+# 方法二：双指针
+class Solution_2:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
+        
+        left, right = 0, 1 
+        length = len(nums)
+
+        while right < length:
+            while right < length and nums[left] == nums[right]:
+                right += 1
+            
+            if right >= length:
+                break
+            
+            left += 1
+            nums[left] = nums[right]
+            right += 1
+        
+        return left + 1
 
 
 #arr =  [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
